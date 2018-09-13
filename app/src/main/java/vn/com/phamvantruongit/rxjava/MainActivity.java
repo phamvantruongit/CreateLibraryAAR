878da51b6a2c98142e75c2e4dd0d0bacf33f523d
PhamTruong <truongpham@bravesoft.com.vn>
2018-09-13 17:48:43 +0700

    Initial commit

---
+++ app/src/main/java/vn/com/phamvantruongit/rxjava/MainActivity.java
@@ -0,0 +1,18 @@
+package vn.com.phamvantruongit.rxjava;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.com.phamvantruongit.mylibrary.LibraryClass;
+
+public class MainActivity extends AppCompatActivity {
+    LibraryClass libraryClass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
